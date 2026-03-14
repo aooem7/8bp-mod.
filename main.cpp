dba825ec5e424ffd7284801350114d25833f0201
aooem7 <sujudisujudi19@gmail.com>
2026-03-14 19:04:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
 #include <jni.h>
-#include <android/log.h>
-
-#define LOG_TAG "MyMod"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
-    LOGI("Mod Loaded Successfully!");
     return JNI_VERSION_1_6;
 }
