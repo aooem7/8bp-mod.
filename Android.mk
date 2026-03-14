01d74558fda372b084dc2b5e54b521cf7ae7ce95
aooem7 <sujudisujudi19@gmail.com>
2026-03-14 19:03:32 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,14 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE    := crashlytics-trampoline
-LOCAL_SRC_FILES := main.cpp \
-                   And64InlineHook.cpp \
-                   SubstrateHook.cpp \
-                   SymbolFinder.cpp
-
-LOCAL_LDLIBS := -llog -landroid
-LOCAL_CPPFLAGS := -std=c++17 -fpermissive
-LOCAL_ARM_MODE := arm
-
+LOCAL_SRC_FILES := main.cpp
+LOCAL_LDLIBS    := -llog
 include $(BUILD_SHARED_LIBRARY)
