4bed59710242c98d2d11b6ef02a122f8a9f39d6e
aooem7 <sujudisujudi19@gmail.com>
2026-03-14 18:49:14 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,16 +1,25 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE     := xhook
-LOCAL_SRC_FILES  := xhook.c \
-                    xh_core.c \
-                    xh_elf.c \
-                    xh_jni.c \
-                    xh_log.c \
-                    xh_util.c \
-                    xh_version.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_CFLAGS     := -Wall -Wextra -fvisibility=hidden
-LOCAL_CONLYFLAGS := -std=c11
-LOCAL_LDLIBS     := -llog
+LOCAL_MODULE    := xhook
+LOCAL_SRC_FILES := xhook.c xh_core.c xh_elf.c xh_jni.c xh_log.c xh_util.c xh_version.c
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := crashlytics-trampoline
+
+LOCAL_SRC_FILES := main.cpp \
+                   And64InlineHook.cpp \
+                   SubstrateDebug.cpp \
+                   SubstrateHook.cpp \
+                   SubstratePosixMemory.cpp \
+                   SymbolFinder.cpp \
+                   dlfcn.cpp
+
+LOCAL_STATIC_LIBRARIES := xhook
+LOCAL_LDLIBS := -llog -landroid -lGLESv2 -lEGL
+
+LOCAL_CPPFLAGS := -std=c++17 -fvisibility=hidden -fpermissive
+LOCAL_ARM_MODE := arm
+
+include $(BUILD_SHARED_LIBRARY)
